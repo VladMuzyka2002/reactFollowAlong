305577c5811dcdc1cf7576e70ce08d4b5ac08436
Vlad Muzyka <angryvlad@gmail.com>
2023-11-16 18:20:14 -0500

    Updated the content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,20 @@
 import { useState } from "react";
 
 function Home(){
-const [name, setName] = useState('mario');
-const [age, setAge] = useState(21);
-
-    const handleClick = (d) => {
-        console.log('hello world', d)
-    }
-
-    const handleClickAgain = (name, e) => {
-        setName('jteetz');
-        setAge(age + 1);
-        console.log('hello ' + name, e.target);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return (
         <div className="home">
-            <h2>This is home</h2>
-            <p>{name} is {age}</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain(name, e)}>Noherone</button>
+            {blogs.map((blog) => (
+                <div className = "blog-previw" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     )
 }
