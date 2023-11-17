d7f7442a8b11e029406918bd8a10f2056034027e
Vlad Muzyka <angryvlad@gmail.com>
2023-11-17 18:42:44 -0500

    Conditional loading message (And reading from json database)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,27 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-    const [name, setName] = useState('mario');
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+      })
+  }, [])
 
-    const handleDelete = (id) => {
-        setBlogs(blogs.filter(blog => blog.id != id))
-    }  
-
-    useEffect(() => {
-        console.log(name)
-    }, [name]);
-
-    useEffect(() => {
-        console.log('MONKY')
-    });
-
-    return (
-        <div className="home">
-            <BlogList blogs={blogs} setBlogs = {setBlogs} title = "All Blogs!" handleDelete = {handleDelete}></BlogList>
-            <button onClick={() => setName('luigi')}>Change Name</button>
-        </div>
-    )
-
-
+  return (
+    <div className="home">
+        {isPending && <div>Loading...</div>}
+        {blogs && <BlogList blogs={blogs} />}
+    </div>
+  );
 }
-
+ 
 export default Home;
