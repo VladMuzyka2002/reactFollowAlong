30deaab1dca08e568c727eaf86ffba6486ddd25b
Vlad Muzyka <angryvlad@gmail.com>
2023-11-16 19:56:41 -0500

    Added new stuff, this time

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
-function Home(){
+const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -9,12 +10,7 @@ function Home(){
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className = "blog-previw" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} setBlogs = {setBlogs} title = "All Blogs!"></BlogList>
         </div>
     )
 }
