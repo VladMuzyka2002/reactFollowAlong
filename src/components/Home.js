3203e14d635aedb60de46ba321dd5ff180632619
Vlad Muzyka <angryvlad@gmail.com>
2023-11-17 22:38:42 -0500

    This doesnt work

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,40 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [name, setName] = useState('mario');
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, [])
 
-    const handleDelete = (id) => {
-        setBlogs(blogs.filter(blog => blog.id != id))
-    }  
-
-    useEffect(() => {
-        console.log(name)
-    }, [name]);
-
-    useEffect(() => {
-        console.log('MONKY')
-    });
-
-    return (
-        <div className="home">
-            <BlogList blogs={blogs} setBlogs = {setBlogs} title = "All Blogs!" handleDelete = {handleDelete}></BlogList>
-            <button onClick={() => setName('luigi')}>Change Name</button>
-        </div>
-    )
-
-
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
-
+ 
 export default Home;
