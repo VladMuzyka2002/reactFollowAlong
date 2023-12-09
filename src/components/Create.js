1dd940e2d2ce998a08d67da91c073abe6eaec0f4
Vlad Muzyka <angryvlad@gmail.com>
2023-12-09 15:16:55 -0500

    Added forms (not submittable)

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Luigi');
+
     return (
-        <div className="create">
+        <div className="create">  
             <h2>Add a New Blog</h2>
+            <form> 
+                <label> Blog Title </label>
+                <input type = "text" required value = {title} onChange={(e)=> setTitle(e.target.value)}></input>
+                <label> Blog body </label>
+                <textarea required value = {body} onChange={(e)=>setBody(e.target.value)}></textarea>
+                <label>Blog Author</label>
+                <select value = {author} onChange={(e)=>setAuthor(e.target.value)}>
+                    <option value = "Mario">Mario</option>
+                    <option value = "Luigi">Luigi</option>
+                </select>
+                <button>Add Blog</button>
+                <p>{author}</p>
+            </form>
         </div>
     );
 }
